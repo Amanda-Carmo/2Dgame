71b6f1d92045b85b364cbdc02e592fc4e81611a6
Amanda Carmo <amanda.rosa.ismart@gmail.com>
2023-04-27 18:07:26 -0300

    camera seguindo o player e parando ao exceder limite de tela

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,6 +8,12 @@ public class CameraController : MonoBehaviour
     public float offset;
     public float offsetSmoothing;
     private Vector3 playerPosition;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +23,33 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    // Verifica se a posição do jogador está dentro dos limites permitidos do mapa.
+    // Se não estiver nos limites de x, para de seguir em X, mas continua em Y e vice-versa
+
+    if (player.transform.position.x > minX && player.transform.position.x < maxX && player.transform.position.y > minY && player.transform.position.y < maxY)
+    {
+        // Atualiza a posição da câmera suavemente.
+        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+    }
+
+    // Limita a posição da câmera apenas no eixo X.
+    else if (player.transform.position.x > minX && player.transform.position.x < maxX)
+    {
+        float clampedY = Mathf.Clamp(player.transform.position.y, minY, maxY);
+        playerPosition = new Vector3(player.transform.position.x, clampedY, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+    }
+    
+    // Limita a posição da câmera apenas no eixo Y.
+    else
+    {
+        float clampedX = Mathf.Clamp(player.transform.position.x, minX, maxX);
+        float clampedY = Mathf.Clamp(player.transform.position.y, minY, maxY);
+        playerPosition = new Vector3(clampedX, clampedY, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+    }
+
+
     }
 }
