6ff40f6f0aea2072fd9dfd95bac90ba00f28d392
GabrielaMitu <gabrielaym@al.insper.edu.br>
2023-04-30 19:55:07 -0300

    pause e unpause

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -9,7 +9,7 @@ public class MenuController : MonoBehaviour
     //public GameObject endPanel; // public GameObject endPanel;
     public GameObject startPanel;
     public GameObject manualPanel;
-    //public GameObject[] pauseUI; // index 0: button, index 1: panel
+    public GameObject[] pauseUI; // index 0: button, index 1: panel
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +17,7 @@ public class MenuController : MonoBehaviour
         //endPanel.SetActive(false);
         startPanel.SetActive(true);
         Time.timeScale = 0;
-        //pauseUI[0].SetActive(false);
+        pauseUI[0].SetActive(false);
         manualPanel.SetActive(false);
     }
 
@@ -32,7 +32,7 @@ public class MenuController : MonoBehaviour
         //endPanel.SetActive(false);
         startPanel.SetActive(true);
         Time.timeScale = 0;
-        //pauseUI[0].SetActive(false);
+        pauseUI[0].SetActive(false);
         manualPanel.SetActive(false);
     }
 
@@ -53,7 +53,7 @@ public class MenuController : MonoBehaviour
     {
         Time.timeScale = 1;
         startPanel.SetActive(false);
-        //pauseUI[0].SetActive(true);
+        pauseUI[0].SetActive(true);
         manualPanel.SetActive(false);
     }
 
@@ -62,19 +62,19 @@ public class MenuController : MonoBehaviour
         Application.Quit();
     }
 
-    // public void Pause()
-    // {
-    //     Time.timeScale = 0; // tempo fica na velocidade zero, ou seja, nada acontece
-    //     pauseUI[0].SetActive(false);
-    //     pauseUI[1].SetActive(true);
-    // }
+    public void Pause()
+    {
+        Time.timeScale = 0; // tempo fica na velocidade zero, ou seja, nada acontece
+        pauseUI[0].SetActive(false);
+        pauseUI[1].SetActive(true);
+    }
 
-    // public void UnPause()
-    // {
-    //     Time.timeScale = 1;
-    //     pauseUI[0].SetActive(true);
-    //     pauseUI[1].SetActive(false);
-    // }
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        pauseUI[0].SetActive(true);
+        pauseUI[1].SetActive(false);
+    }
 
     // public void LoseGame() 
     // {
